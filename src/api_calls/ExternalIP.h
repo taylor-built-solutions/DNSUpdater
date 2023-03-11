8843f65b329c9906a6f43110b2418832a5fc0656
taylor-built-solutions.com <taylor@taylorbuiltsolutions.com>
2023-03-11 08:15:23 -0800

    Use the latest conan that isn't conan 2. Move the code for getting the external IP out of main.

--- src/api_calls/ExternalIP.h
+++ src/api_calls/ExternalIP.h
@@ -1,13 +1,15 @@
 #include <cpr/cpr.h>
 #include <nlohmann/json.hpp>
+#include <spdlog/spdlog.h>
+using json = nlohmann::json;
 
 class ExternalIP
 {
 public:
-    std::string GetExternalIPAddress();
-    std::string IPAddress() { return externalIP; }
+  std::string GetExternalIPAddress();
+  std::string IPAddress() const { return externalIP; }
 
 private:
-    std::string externalIP;
-    json externalIPJson;
-}
+  std::string externalIP;
+  json externalIPJson;
+};
