cc02e71b4612e3b9b994244f32ad941e19f62505
taylor-built-solutions.com <taylor@taylorbuiltsolutions.com>
2023-03-31 22:56:02 +0000

    Change the name of the executable. Start tests

---
+++ test/currentIP_Tests.cpp
@@ -0,0 +1,7 @@
+#include "currentIP.h"
+#include <catch2/catch_test_macros.hpp>
+
+TEST_CASE("currentIP_UpdateCurrentIP_Test", "[currentIP]")
+{
+  SECTION("Default File Name, No previously existing file") {}
+}
